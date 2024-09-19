ab604a39b947326b92df4aaaebcc4f6ed667a4fc
Meooooow123 <186615@stud.hvl.no>
2024-09-19 23:53:40 +0200

    Oppgave 03

--- src/no/hvl/dat100/o3.java
+++ src/no/hvl/dat100/o3.java
@@ -1,10 +1,36 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+ 
+
 public class o3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	
+	 
+		public static void main(String[] args) {
+		
+			        String tallInput = showInputDialog("Skriv inn et heltall n > 0 for å beregne n!");
+	 
+			       
+			        int tall = Integer.parseInt(tallInput);
+	 
+			        
+			        if (tall > 0) {
+			           
+			            long resultat = 1;
+	 
+			           
+			            for (int i = 1; i <= tall; i++) {
+			                resultat *= i;
+			            }
+	 
+			            
+			            showMessageDialog(null, tall + "! = " + resultat);
+			        } else {
+			           
+			            showMessageDialog(null, "Tallet må være større enn 0.");
+			        }
+			    }
+			}
+	 
